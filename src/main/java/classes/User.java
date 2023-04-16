07bd4bf64a9cbeab312c7e4c871231333ecacd38
andyBzg <andr.bzg@gmail.com>
2023-04-16 12:59:25 +0200

    Initial commit

---
+++ src/main/java/classes/User.java
@@ -0,0 +1,21 @@
+package classes;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private LocalDate dateOfBirth;
+
+
+    public User(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
