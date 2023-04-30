fd63663dc8c9d18a35704d649e3f2a7f388a5d59
andyBzg <andr.bzg@gmail.com>
2023-04-30 11:26:39 +0200

    commit

--- src/main/java/classes/FileService.java
+++ src/main/java/classes/FileService.java
@@ -1,6 +1,5 @@
 package classes;
 
-import enums.EducationLevel;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.*;
@@ -16,9 +15,7 @@ public class FileService {
         this.file = file;
     }
 
-    /**
-     * Добавляет названия организаций из файла в лист
-     **/
+
     public List<String> addStringsFromFileToList() {
         List<String> stringList = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
@@ -34,44 +31,5 @@ public class FileService {
         return stringList;
     }
 
-    //TODO удалить служебные методы
-    /**
-     * Служебный метод для перезаписи файла в новый файл (бекап, перезапись, запись с измененными строчками)
-     * **/
-    public void rewriteToNewFile(String inputFile, String outputFile) {
-        try {
-            try(BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))){
-                String line;
-                while((line = bufferedReader.readLine()) != null) {
-                    bufferedWriter.write(line);
-                    bufferedWriter.newLine();
-                }
-            }
-        } catch (IOException e) {
-            log.error("rewriteToNewFile: " + e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Служебный метод
-     * Добавляет "ограничения по возрасту" для каждой организации рандомно
-     **/
-    public void rewriteToNewFileWithRandomAgeLimit(String inputFile, String outputFile) {
-        try {
-            try(BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))){
-                String line;
-                while((line = bufferedReader.readLine()) != null) {
-                    bufferedWriter.write(EducationLevel.getRandomLevel().getAgeRange() + ", " + line);
-                    bufferedWriter.newLine();
-                }
-            }
-        } catch (IOException e) {
-            log.error("rewriteToNewFileWithRandomAgeLimit: " + e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
 
 }
