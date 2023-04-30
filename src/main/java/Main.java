7f8a9540ca256f0a79e1c03f9382dfb43c766419
andyBzg <andr.bzg@gmail.com>
2023-04-30 11:19:37 +0200

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,29 @@
+import classes.*;
+import lombok.extern.log4j.Log4j2;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Log4j2
 public class Main {
     public static void main(String[] args) {
 
+        String file = "src/main/resources/Universities.txt";
+        FileService fileService = new FileService(file);
+        List<String> stringsList = fileService.addStringsFromFileToList();
+
+        Institution institution = new Institution();
+        List<Institution> institutionList = institution.convertStringsToObjects(stringsList);
+
+        BirthdateRequest birthdateRequest = new BirthdateRequest();
+        LocalDate birthDate = birthdateRequest.getUserBirthdate();
+
+        User user = new User(birthDate);
+
+        List<Institution> suitableList = institution.getSuitableInstitutionByUserAge(institutionList, user.getAge());
+
+        MessagePrinter message = new MessagePrinter();
+
+        message.printWithCount(suitableList);
     }
 }
