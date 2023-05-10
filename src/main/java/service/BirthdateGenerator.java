52b808e2c645767e626fa9abda83ecb4d60a6770
andyBzg <andr.bzg@gmail.com>
2023-05-10 23:25:43 +0200

    commit

--- src/main/java/service/BirthdateGenerator.java
+++ src/main/java/service/BirthdateGenerator.java
@@ -7,13 +7,17 @@ import output.MessagePrinter;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Log4j2
 public class BirthdateGenerator {
 
+    private static final String PLACEHOLDER = "%s-%s-%s";
     private final BirthdateReader birthdateReader;
     private final MessagePrinter message;
-    private static final String PLACEHOLDER = "%s-%s-%s";
+    private String day;
+    private String month;
+    private String year;
 
 
     public BirthdateGenerator() {
@@ -22,11 +26,16 @@ public class BirthdateGenerator {
     }
 
 
-    public LocalDate createBirthdate() {
+    public void initializeBirthdateRequest() {
         message.printBirthdateRequestMessage();
-        String day = birthdateReader.readDay();
-        String month = birthdateReader.readMonth();
-        String year = birthdateReader.readYear();
+        day = birthdateReader.readDay();
+        month = birthdateReader.readMonth();
+        year = birthdateReader.readYear();
+    }
+
+
+    public LocalDate createBirthdate() {
+        initializeBirthdateRequest();
         try {
             String date = String.format(PLACEHOLDER, year, month, day);
             LocalDate dateOfBirth = LocalDate.parse(date);
@@ -42,7 +51,6 @@ public class BirthdateGenerator {
         }
     }
 
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -51,13 +59,19 @@ public class BirthdateGenerator {
         BirthdateGenerator that = (BirthdateGenerator) o;
 
         if (!birthdateReader.equals(that.birthdateReader)) return false;
-        return message.equals(that.message);
+        if (!message.equals(that.message)) return false;
+        if (!Objects.equals(day, that.day)) return false;
+        if (!Objects.equals(month, that.month)) return false;
+        return Objects.equals(year, that.year);
     }
 
     @Override
     public int hashCode() {
         int result = birthdateReader.hashCode();
         result = 31 * result + message.hashCode();
+        result = 31 * result + (day != null ? day.hashCode() : 0);
+        result = 31 * result + (month != null ? month.hashCode() : 0);
+        result = 31 * result + (year != null ? year.hashCode() : 0);
         return result;
     }
 }
---
+++ src/test/java/classes/BirthdateGeneratorTest.java
@@ -0,0 +1,27 @@
+package classes;
+
+import org.junit.jupiter.api.Test;
+import service.BirthdateGenerator;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BirthdateGeneratorTest {
+
+    @Test
+    void createBirthdate_positive() {
+
+    }
+
+    @Test
+    void createBirthdate_negative() {
+
+    }
+
+    @Test
+    void createBirthdate_throwsException() {
+
+    }
+
+}
